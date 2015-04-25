aef74c7313c103a029a9eb5b2a5a5213933916f9
Kris Schaller <kris.schaller@gmail.com>
2015-04-24 17:30:01 -0700

    Initial commit

---
+++ smartapps/smartthings/delete-me.groovy
@@ -0,0 +1,14 @@
+/**
+ *  Delete Me
+ *
+ *  Author: SmartThings
+ */
+definition(
+    name: "Delete Me",
+    namespace: "smartthings",
+    author: "SmartThings",
+    description: "Turn on or off any number of switches when an existing switch is tapped twice in a row.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet@2x.png"
+)
